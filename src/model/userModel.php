06a45fab58538d8d719138f7e0842f958c842b07
MatiasBerrueta <matias.berrueta1@gmail.com>
2025-09-02 16:53:44 -0300

    Refactorizacion de userModel

--- src/model/userModel.php
+++ src/model/userModel.php
@@ -2,24 +2,20 @@
 class usuario {
     private $conexion;
 
-    public $id;
-    public $nombre;
-    public $email;
-    public $contrasenaPlana;
-    public $contrasenaHasheada;
+    private $nombre;
+    private $email;
+    private $contrasenaHasheada;
 
-    public function __construct($db) {
+    public function __construct($email, $contrasenaPlana, $db) {
+        // sanitizar datos del usuario, se eliminan tags de html y convierte caracteres especiales en entidades html
+        $this->email = htmlspecialchars(strip_tags($email));
+        $this->contrasenaHasheada = password_hash($contrasenaPlana, PASSWORD_BCRYPT);
         $this->conexion = $db;
     }
 
     public function crearUsuario() {
         $query = "INSERT INTO usuarios (nombre, email, contrasena) VALUES (:nombre, :email, :contrasena)";
         $stmt = $this->conexion->prepare($query);
-
-        // sanitizar datos del usuario, se eliminan tags de html y convierte caracteres especiales en entidades html
-        $this->nombre = htmlspecialchars(strip_tags($this->nombre));
-        $this->email = htmlspecialchars(strip_tags($this->email));
-        $this->contrasenaHasheada = password_hash($this->contrasenaPlana, PASSWORD_BCRYPT);
 
         $stmt->bindParam(':nombre', $this->nombre);
         $stmt->bindParam(':email', $this->email);
@@ -28,21 +24,32 @@ class usuario {
         return $stmt->execute();
     }
 
-    public function autenticarUsuario() {
+    public function autenticarUsuario($contrasenaPlana) {
         $query = "SELECT * FROM usuarios WHERE email = :email";
         $stmt = $this->conexion->prepare($query);
 
-        $this->email = htmlspecialchars(strip_tags($this->email));
         $stmt->bindParam(':email', $this->email);
 
         $stmt->execute();
         $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
         
-        if($usuario && password_verify($this->contrasenaPlana, $usuario['contrasena'])) {
+        if($usuario && password_verify($contrasenaPlana, $usuario['contrasena'])) {
             return $usuario;
         } else {
             return false;
         }
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getEmail() {
+        return $this->nombre;
+    }
+
+    public function getContrasena() {
+        return $this->nombre;
     }
 }
 ?>
