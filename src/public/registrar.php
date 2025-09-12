64dade48e0da3dccb472fdc84a81919113a7e021
MatiasBerrueta <matias.berrueta1@gmail.com>
2025-09-12 20:15:12 -0300

    Cambio de ruta de estilos.

--- src/public/registrar.php
+++ src/public/registrar.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/registro.css">
     <script src="js/validacionRegistro.js" defer></script>
     <title>Registro</title>
 </head>
