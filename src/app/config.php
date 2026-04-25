7011adddf85c395cb8ca455c60a0ba3c952318ed
Matias Berrueta <matias.berrueta1@gmail.com>
2026-04-25 14:19:54 -0300

    Logs separados por dia.

--- src/app/config.php
+++ src/app/config.php
@@ -5,4 +5,4 @@ define("PUBLIC_ROOT", dirname(__DIR__) . '/public/');
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
-ini_set('error_log', APP_ROOT . '../logs/php_error.log');
+ini_set('error_log', APP_ROOT . '../logs/php_error_' . date('Y-m-d') . '.log');
