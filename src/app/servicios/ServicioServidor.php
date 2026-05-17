b005ea912729ad88202591bccb18b7afc5d42575
Matias Berrueta <matias.berrueta1@gmail.com>
2026-05-16 21:00:27 -0300

    Tipos agregados a variables y parametros

--- src/app/servicios/ServicioServidor.php
+++ src/app/servicios/ServicioServidor.php
@@ -1,17 +1,17 @@
 <?php
 
 class ServicioServidor {
-    private $pterodactylCliente;
-    private $repositorioServidor;
-    private $repositorioUsuario;
+    private pterodactylClientApi $pterodactylCliente;
+    private RepositorioServidor $repositorioServidor;
+    private RepositorioUsuario $repositorioUsuario;
 
-    public function __construct($pterodactylCliente, $repositorioServidor, $repositorioUsuario) {
+    public function __construct(pterodactylClientApi $pterodactylCliente,  RepositorioServidor $repositorioServidor, RepositorioUsuario $repositorioUsuario) {
         $this->pterodactylCliente = $pterodactylCliente;
         $this->repositorioServidor = $repositorioServidor;
         $this->repositorioUsuario = $repositorioUsuario;
     }
 
-    public function obtenerServidoresPterodactyl($idUsuario) {
+    public function obtenerServidoresPterodactyl(int $idUsuario) {
         $clientKey = $this->repositorioUsuario->obtenerClientKey($idUsuario);
         $servidoresPterodactyl = $this->pterodactylCliente->obtenerServidores($clientKey);
         $juegos = $this->repositorioServidor->obtenerJuegosServidores($idUsuario);
@@ -24,7 +24,7 @@ class ServicioServidor {
         }, $servidoresPterodactyl);
     }
 
-    public function obtenerServidorPterodactyl($idServidor, $idUsuario) {
+    public function obtenerServidorPterodactyl(int $idServidor, int $idUsuario) {
         $clientKey = $this->repositorioUsuario->obtenerClientKey($idUsuario);
         $servidor = $this->pterodactylCliente->obtenerServidor($idServidor, $clientKey);
         $juego = $this->repositorioServidor->obtenerJuegoServidor($idUsuario, $servidor['identifier']);
@@ -32,7 +32,7 @@ class ServicioServidor {
         return array_merge($servidor, $juego ?? []);
     }
     
-    public function obtenerRecursosServidorPterodactyl($id, $idUsuario) {
+    public function obtenerRecursosServidorPterodactyl(int $id, int $idUsuario) {
         $clientKey = $this->repositorioUsuario->obtenerClientKey($idUsuario);
         return $this->pterodactylCliente->obtenerRecursosServidor($id, $clientKey);
     }
